dcc74f23f28cc2cae19593c325267ec68ab03273
-akku- <akkun11.open@gmail.com>
2025-10-25 14:38:39 +0900

    prompt tuning

--- src/index.ts
+++ src/index.ts
@@ -125,12 +125,11 @@ const systemPrompt = [
   '絵文字の使用は控えめにしてください。',
   'メッセージの返答にメンションを入れる必要はありません。',
   'みんなには内緒ですが、あなたは実はAIです。',
-  '"function callの関数" の略として func:xxxx と言う場合があります。',
   'function callの後にレスポンスがなかった場合は、エラーが発生したものと考えてください。',
-  'メッセージのURLは guildId, channelId, messageId (repliesオブジェクト等に含まれる) から "https://discord.com/channels/${guildId}/${channelId}/${messageId}" として生成できます。リプライ先を取得する際などは、この方法でURLを取得した後に、func:fetch_message でメッセージを取得できます。',
+  'メッセージのURLは guildId, channelId, messageId (repliesオブジェクト等に含まれる) から "https://discord.com/channels/${guildId}/${channelId}/${messageId}" として生成できます。リプライ先を取得する際などは、この方法でURLを取得した後に、function call: fetch_message でメッセージを取得できます。',
   `あなたへのメッセージはJSON形式で、次のJSON Schema \`${JSON.stringify(discordMessageSchema)}\` の形式で与えられるはずです。`,
   '特に言及がない限り、タイムゾーンはJSTを使用してください。',
-  'メッセージ履歴をまとめたい場合などは func:fetch_messages_history を使って複数のメッセージを取得してみてください。'
+  'メッセージ履歴をまとめたい場合などは function call: fetch_messages_history を使って複数のメッセージを取得してみてください。'
 ];
 
 addCommand(new SlashCommandBuilder().setName('enableai').setDescription('enable AI feature in this channel'), async i => {
